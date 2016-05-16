455c08907f11025ba92fc9cfd7be0ecec0dfc614
Mike Mueller <mikemueller04@hotmail.com>
2016-05-16 18:34:27 -0400

    refactor for DRY

--- berlin_clock.rb
+++ berlin_clock.rb
@@ -7,43 +7,31 @@ class BerlinClock
 
   def one_minute_row
     row = ''
-
     (@minute % 5).times { row += 'Y' }
-
-    row += 'O' until row.length == 4
-
+    row = append_off_lights(row, 4)
     row
   end
 
   def five_minute_row
     row = ''
-
     (@minute / 5).times do
       row += ((row.length + 1) % 3 == 0) ? 'R' : 'Y'
     end
-
-    row += 'O' until row.length == 11
-
+    row = append_off_lights(row, 11)
     row
   end
 
   def one_hour_row
     row = ''
-
     (@hour % 5).times { row += 'R' }
-
-    row += 'O' until row.length == 4
-
+    row = append_off_lights(row, 4)
     row
   end
 
   def five_hour_row
     row = ''
-
     (@hour / 5).times { row += 'R' }
-
-    row += 'O' until row.length == 4
-
+    row = append_off_lights(row, 4)
     row
   end
 
@@ -57,5 +45,10 @@ class BerlinClock
     one_hour_row +
     five_minute_row +
     one_minute_row
+  end
+
+  def append_off_lights(row, length)
+    row += 'O' until row.length == length
+    return row
   end
 end
